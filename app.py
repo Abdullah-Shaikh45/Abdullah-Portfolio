814660407c319c5c85286ab5df981fe0f1a77eed
Abdullah Shaikh <abdullah.shaikh76ir@gmail.com>
2025-07-24 19:28:12 +0530

    add animation

--- app.py
+++ app.py
@@ -1,29 +1,13 @@
-from flask import Flask, render_template, request, redirect, flash
+from flask import Flask, render_template
 from datetime import datetime
-from dotenv import load_dotenv
 import os
 
 app = Flask(__name__)
-load_dotenv()
-app.secret_key = os.getenv('SECRET_KEY')
 
 
 @app.route('/')
 def index():
     return render_template('index.html', year=datetime.now().year)
-
-
-@app.route('/contact', methods=['POST'])
-def contact():
-    name = request.form['name']
-    email = request.form['email']
-    message = request.form['message']
-
-    print("New message from:", name, email)
-    print("Message:", message)
-
-    flash("Thanks for contacting me, " + name + "!", "success")
-    return redirect('/')
 
 
 if __name__ == '__main__':
